9edb5a6512c534cb675294f2e807ba733729f6f1
ARYA ADESH BHOSLE <110241610+Aryab1703@users.noreply.github.com>
2023-01-19 21:55:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+/*===== SHOW MENU ======*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== CHANGE BACKGROUND HEADER =====*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if (this.scrollY >= 200) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*===== SHOW SCROLL TOP =====*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+    if (this.scrollY >= 660) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop)
+
+/*===== GSAP ANIMATION =====*/
+gsap.from('.home__img', { opacity: 0, duration: 2, delay: .5, x: 60 })
+gsap.from('.home__data', { opacity: 0, duration: 2, delay: .8, y: 25 })
+
+
+// $(document).ready(function() {
+//     $(".sidemenu-toggler").click(function() {
+//         $(".sidemenu").addClass("active");
+//     });
+
+//     $(".close").click(function() {
+//         $(".sidemenu").removeClass("active");
+//     });
+
+//     $(window).scroll(function() {
+//         var sc = $(window).scrollTop();
+//         if (sc > 150) {
+
+//         }
+//     })
+// })
